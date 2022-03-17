f5883b4cba1b7ff0bfbf36c831ffccb4a3eedca2
mouyong <my24251325@gmail.com>
2022-03-17 21:28:35 +0800

    feat: Init.

---
+++ README.md
@@ -0,0 +1,7 @@
+## 引入的第三方包
+
+|包名|作用|
+|---|---|
+|zhenmu/support|基础支持|
+|mouyong/validate|表单验证|
+|webman/console|web 控制台插件|
---
+++ src/Controllers/WebmanBaseController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Mouyong\WebmanInitTemplate\Controllers;
+
+use ZhenMu\Support\Traits\WebmanResponseTrait;
+
+class WebmanBaseController
+{
+    use WebmanResponseTrait;
+}
